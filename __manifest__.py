dfb8c2f23407ccd6ef4e643391a92b39545e7095
Anh Tuan <silerspirit2001@gmail.com>
2022-08-22 14:46:29 +0700

    cau1

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "mytest_2",
+    'summary': "Manage books easily",
+    'description': """
+Manage Library
+==============
+Description related to library.
+""",
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '15.0.13.0.1.0.1',
+    'depends': ['base', 'account', 'sale_management'],
+    'data': ['views/product_template_inherit.xml'],
+    'demo': ['data/demo.xml'],
+}
