4ff24f3eb8e6c79a8e5b55de6a51540c996c175a
Anh Tuan <silerspirit2001@gmail.com>
2022-08-22 15:25:22 +0700

    cau2

--- __manifest__.py
+++ __manifest__.py
@@ -11,6 +11,8 @@ Description related to library.
     'category': 'Uncategorized',
     'version': '15.0.13.0.1.0.1',
     'depends': ['base', 'account', 'sale_management'],
-    'data': ['views/product_template_inherit.xml'],
+    'data': ['security/groups.xml',
+             'security/ir.model.access.csv',
+             'views/product_template_inherit.xml'],
     'demo': ['data/demo.xml'],
 }
